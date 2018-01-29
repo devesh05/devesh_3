550735e274934394ee23367db83c0ce2a484c36d
devesh05 <35624629+devesh05@users.noreply.github.com>
2018-01-29 22:14:17 +0530

    Update devesh1.c

--- devesh1.c
+++ devesh1.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 void main()
 {
-    int n,a,d,sum;
+    int n,a,d,s=0;
     scanf("%d %d %d",&n,&a,&d);
-    sum=n*(2*a+(n-1)*d);
-    sum=sum/2;
-    printf("%d",sum);
+    s=n*(2*a+(n-1)*d);
+    s=s/2;
+    printf("%d",s);
 }
